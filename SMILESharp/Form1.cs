6cad4f7d83f259065f4ef9d64a69181321264c09
Zero-One101 <stephenbadrick@gmail.com>
2016-02-14 21:27:38 +0000

    Successfully draw garbage to screen
    Praise be to xdaniel for figuring this one out for me!

--- SMILESharp/Form1.cs
+++ SMILESharp/Form1.cs
@@ -31,29 +31,33 @@ namespace SMILESharp
         {
             var graphicsData = rom.GetGraphicsData();
             var buffer = new Bitmap(256, 256, PixelFormat.Format4bppIndexed);
-            buffer.Palette.Entries[0] = Color.FromArgb(255, 0, 0, 0);
-            buffer.Palette.Entries[1] = Color.White;
-            buffer.Palette.Entries[2] = Color.Red;
-            buffer.Palette.Entries[3] = Color.Green;
-            buffer.Palette.Entries[4] = Color.Yellow;
-            buffer.Palette.Entries[5] = Color.Blue;
-            buffer.Palette.Entries[6] = Color.Purple;
-            buffer.Palette.Entries[7] = Color.Aqua;
-            buffer.Palette.Entries[8] = Color.Black;
-            buffer.Palette.Entries[9] = Color.White;
-            buffer.Palette.Entries[10] = Color.Red;
-            buffer.Palette.Entries[11] = Color.Green;
-            buffer.Palette.Entries[12] = Color.Yellow;
-            buffer.Palette.Entries[13] = Color.Blue;
-            buffer.Palette.Entries[14] = Color.Purple;
-            buffer.Palette.Entries[15] = Color.Aqua;
+            var palette = buffer.Palette;
+            palette.Entries[0] = Color.FromArgb(255, 0, 0, 0);
+            palette.Entries[1] = Color.White;
+            palette.Entries[2] = Color.Red;
+            palette.Entries[3] = Color.Green;
+            palette.Entries[4] = Color.Yellow;
+            palette.Entries[5] = Color.Blue;
+            palette.Entries[6] = Color.Purple;
+            palette.Entries[7] = Color.Aqua;
+            palette.Entries[8] = Color.Black;
+            palette.Entries[9] = Color.White;
+            palette.Entries[10] = Color.Red;
+            palette.Entries[11] = Color.Green;
+            palette.Entries[12] = Color.Yellow;
+            palette.Entries[13] = Color.Blue;
+            palette.Entries[14] = Color.Purple;
+            palette.Entries[15] = Color.Aqua;
+            buffer.Palette = palette;
 
-            var boundsRect = new Rectangle(pbxCanvas.Location.X, pbxCanvas.Location.Y, 128, 128);
+            var boundsRect = new Rectangle(0, 0, 256, 256);
             var bmpData = buffer.LockBits(boundsRect, ImageLockMode.ReadWrite, PixelFormat.Format4bppIndexed);
 
             var ptr = bmpData.Scan0;
             Marshal.Copy(graphicsData, 0, ptr, graphicsData.Length);
             buffer.UnlockBits(bmpData);
+
+            pbxCanvas.Image = buffer;
 
             Invalidate();
         }
